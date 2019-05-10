4941251728f7fb61a6fb9505ace24ebad80283d2
Scott Lindauer <smlindau@ncsu.edu>
2019-05-09 22:47:52 -0400

    Create rotateLosslessImage.m

---
+++ rotateLosslessImage.m
@@ -0,0 +1,82 @@
+function rotatedImage = rotateLosslessImage(baseImage,rotateAngle)
+
+% Determine the size of the image and begin a loop through each row of the image
+imageSize = size(baseImage);
+rotatedImage = zeros(imageSize(1),imageSize(2),imageSize(3));
+for row = 1:imageSize(1)
+	
+	% Step through each pixel in the row and compute a new non-integer pixel location
+  	for col = 1:imageSize(2)
+    
+		newPixelLoc(col,:) = [1 + (col-8)*cosd(rotateAngle) + (row-1)*sind(rotateAngle),...
+			1 + (col-8)*sind(rotateAngle) - (row-1)*cosd(rotateAngle)];
+			
+		% Set a test rectangle around the rotated pixel location
+		testRectangle = getTestRectangle(newPixelLoc(col,1),newPixelLoc(col,2),rotationAngle);
+		
+		% For each pixel within 1 px of the pixel location, step through and determine the overlap
+		for i = floor(min(testRectangle(:,1))):ceil(max(testRectangle(:,1)))
+			for j = floor(min(testRectangle(:,2))):ceil(max(testRectangle(:,2)))
+				
+				% Define the rectangle pixel to check for overlaps
+				overlapPixel = [i-.5 j-.5]' [i+.5 j-.5]' [i+.5 j+.5]' [i-.5 j+.5];
+				
+				% Determine the area overlap of the pixels
+				areaOverlap = getAreaOverlap(testRectange,overlapPixel);
+				
+				% Assign the pixel value for the rotated image
+				rotatedImage(row,col,:) = rotatedImage(row,col,:) + areaOverlap*baseImage(j,i,:);
+			end
+		end
+  	end
+end
+
+end
+
+%%%%%%%%%% --- Anonymous Functions --- %%%%%%%%%%
+
+%% Anonymous function for generating test rectangles
+function rect = getTestRectangle(x,y,angle)
+
+% Set the initial pixel location to create a rectangle around
+point = [x,y];
+
+% Generate a unit vector using the rotation angle
+[xUnit, yUnit] = pol2cart(angle*pi/180,1);
+unitVector = [xUnit, yUnit];
+
+% Set the conjugate unit vector to complete the rotation matrix
+conjUnitVector = [unitVector(2),-unitVector(1)];
+
+% Evaluate the rotation matrix 
+rowA = unitVector * .5;
+rowB = conjUnitVector * .5;
+
+% Assign the edges of the rectangle based on the rotation matrix
+rect(1,:) = point + rowA + rowB;
+rect(2,:) = point + rowA - rowB;
+rect(3,:) = point - rowA - rowB;
+rect(4,:) = point - rowA + rowB;
+
+end
+
+%% Anonymous function for determining the area overlap
+% Uses the points of two quadrangles to determine the area overlap of two squares
+function overlap = getAreaOverlap(testQuadrangle,overlapPixelQuadrangle)
+
+% Assign the x and y coordinates of the test quadrangle to new variables
+% and rotate them into a clockwise alignment; do the same for the overlap
+% pixel quadrangle
+xa = testQuadrangle(:,1);
+ya = testQuadrangle(:,2);
+[xa,ya] = poly2cw(xa,ya);
+xb = overlapPixelQuadrangle(:,1);
+yb = overlapPixelQuadrangle(:,2);
+[xb,yb] = poly2cw(xb,yb);
+
+% Use matlab's polybool function to determine the total overlap of the two
+% quadrangles in clockwise alignment
+[xc,yc] = polybool('and',xa,ya,xb,yb);
+overlap = polyarea(xc,yc);
+
+end
